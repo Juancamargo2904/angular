7d011650bf2761aee039e27ffbcdf3ff1b1c45e6
juan <juan.camargos@cun.edu.co>
2024-09-10 19:45:33 -0500

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistrarProductosComponent } from './registrar-productos/registrar-productos.component';
+import { RegistrarUsuariosComponent } from './registrar-usuarios/registrar-usuarios.component';
+import { LoginComponent } from './login/login.component';
+import { ProductosComponent } from './productos/productos.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { NavbarPrincipalComponent } from './navbar-principal/navbar-principal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+
+  {path: 'registrarproducto', component:RegistrarProductosComponent  },
+  {path: 'registrarusuarios', component:RegistrarUsuariosComponent  },
+  {path: 'productos', component:ProductosComponent},
+  {path: 'usuarios', component:UsuariosComponent},
+  {path: 'login', component:LoginComponent},
+  {path: 'navbar', component:NavbarPrincipalComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrarProductosComponent } from './registrar-productos/registrar-productos.component';
+import { RegistrarUsuariosComponent } from './registrar-usuarios/registrar-usuarios.component';
+import { NavbarPrincipalComponent } from './navbar-principal/navbar-principal.component';
+import { LoginComponent } from './login/login.component';
+import { ProductosComponent } from './productos/productos.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrarProductosComponent,
+    RegistrarUsuariosComponent,
+    NavbarPrincipalComponent,
+    LoginComponent,
+    ProductosComponent,
+    UsuariosComponent,
   ],
   imports: [
     BrowserModule,
